8abfe451d3fa2d33cd98ef43db920ca1c4a2c3f3
Siddharth Saurav <sid.saurav11@gmail.com>
2024-06-14 14:46:28 +0530

    init commit

---
+++ src/main/java/com/sidsaurav/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.sidsaurav.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
